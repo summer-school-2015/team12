c19551fd63ec81590a535d6274d1eda5cae35777
MCdanja <danakkurat@mail.ru>
2015-06-03 12:04:52 +0300

    helloex

---
+++ helloex/Hello.java
@@ -0,0 +1,8 @@
+package team12.helloex;
+import team12.helloex.Printer;
+class HelloWorld {
+    public static void main(String[] args) {
+
+	Printer.print();
+    }
+}
---
+++ helloex/Printer.java
@@ -0,0 +1,7 @@
+package team12.helloex;
+class Printer {
+	public static void print()
+	{
+		System.out.println("Hello World!");
+	}
+}
