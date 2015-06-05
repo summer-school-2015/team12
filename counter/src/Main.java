8f4e4716ea0b0e97e7e0c1433f1886cd8d710690
MCdanja <danakkurat@mail.ru>
2015-06-05 12:31:27 +0300

    counter

--- counter/src/Main.java
+++ counter/src/Main.java
@@ -8,43 +8,70 @@ static int nsymb = 0;
     static int nword = 0;
 
 
-    public static void main(String[] args) throws IOException{
-        String path1 = args[0].substring(3);
-        String path2 = args[1].substring(3);
+    public static void main(String[] args) throws IOException {
+
+        if (args.length == 2) {
+            if (args[0].contains("if=")) {
+                try (FileInputStream fin = new FileInputStream(args[0].substring(3))) {
+                    ReadFile(args[0].substring(3));
+                } catch (IOException er) {
+                    System.out.println(er.getMessage());
+                }
+                try (FileOutputStream fin = new FileOutputStream(args[1].substring(3))) {
+                    PrintFile(args[1].substring(3));
+                } catch (IOException ero) {
+                    System.out.println(ero.getMessage());
+                }
 
 
-        if (args[0].contains(path1)) {
-          try(  FileInputStream fin = new FileInputStream(path1)) {
-              ReadFile();
-              PrintConsole();
-          }
-            catch (IOException er)
-            {
-                System.out.println(er.getMessage());
+            } else {
+                try (FileInputStream fin = new FileInputStream(args[1].substring(3))) {
+                    ReadFile(args[1].substring(3));
+                } catch (IOException er) {
+                    System.out.println(er.getMessage());
+                }
+                try (FileOutputStream fin = new FileOutputStream(args[0].substring(3))) {
+                    PrintFile(args[0].substring(3));
+                } catch (IOException ero) {
+                    System.out.println(ero.getMessage());
+                }
             }
         }
-        if (args[0].contains(path1) && args[1].contains(path2)) {
-            ReadFile();
-            PrintFile();
+            if (args.length == 1) {
+                if (args[0].contains("if=")) {
+                    try (FileInputStream fin = new FileInputStream(args[0].substring(3))) {
+                        ReadFile(args[0].substring(3));
+                    } catch (IOException er) {
+                        System.out.println(er.getMessage());
+                    }
+                    PrintConsole();
+                } else {
+                    ReadConsole();
+                    try (FileOutputStream fin = new FileOutputStream(args[0].substring(3))) {
+                        PrintFile(args[0].substring(3));
+                    } catch (IOException er) {
+                        System.out.println(er.getMessage());
+                    }
+
+                }
+            }
+            if (args.length == 0) {
+                ReadConsole();
+                PrintConsole();
+            }
+
+
         }
 
-        if (args[1].contains(path2)) {
-            ReadConsole();
-            PrintFile();
-        }
 
-        if (args.length == 0) {
-            ReadConsole();
-            PrintConsole();
-        }
-    }
 
-    public static void ReadFile() {
+    public static void ReadFile(String path1) {
         String str = "";
         String all = "";
-        try(BufferedReader fis = new BufferedReader(new FileReader("C:\\Users\\User\\IdeaProjects\\counters\\if.txt"))){
-           str=fis.readLine();
-            while(!str.equals("")){
+        try(BufferedReader fis = new BufferedReader(new FileReader(path1))){
+
+            while((str = fis.readLine()) != null){
+
                 nstr++;
                 all += str + " ";
                 nsymb += str.length();
@@ -58,15 +85,42 @@ static int nsymb = 0;
     }
 
     static void PrintConsole() {
-
+        System.out.println(nstr + " " + nword + " " + nsymb);
     }
 
     static void ReadConsole() {
-        System.out.println(nstr + " " + nword + " " + nsymb);
+        String str = "";
+        String all = "";
+        try(BufferedReader fis = new BufferedReader(new InputStreamReader(System.in))){
+
+            while(!((str = fis.readLine()).equals(""))){
+
+                nstr++;
+                all += str + " ";
+                nsymb += str.length();
+            }
+            StringTokenizer fas = new StringTokenizer(all);
+            nword = fas.countTokens();
+        }
+        catch(IOException e){
+            System.out.println(e.getMessage());
+        }
 
     }
 
-    static void PrintFile() {
+    static void PrintFile(String path2) {
+        try(BufferedWriter fot = new BufferedWriter(new FileWriter(path2)))
+        {
+            fot.write("Strings = " + nstr);
+            fot.newLine();
+            fot.write("Words = " + nword);
+            fot.newLine();
+            fot.write("Symbols = " + nsymb);
+        }
+        catch (IOException err)
+        {
+            System.out.println(err.getMessage());
+        }
     }
 
 }
